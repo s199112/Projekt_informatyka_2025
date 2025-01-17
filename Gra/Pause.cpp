8c18dedeacbd1edab4d722d7326bb0f43b6d118c
s199112 <s199112@student.pg.edu.pl>
2025-01-17 23:20:06 +0100

    Create Pause.cpp

---
+++ Gra/Pause.cpp
@@ -0,0 +1,48 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <iostream>
+
+class PauseMenu {
+private:
+    sf::Font font;
+    sf::Text pauseText;
+    sf::Text continueText;
+    sf::Text quitText;
+
+    sf::RectangleShape background;
+
+public:
+    PauseMenu(sf::RenderWindow& window) {
+        if (!font.loadFromFile("arial.ttf")) {
+            std::cerr << "Failed to load font.\n";
+        }
+
+        pauseText.setFont(font);
+        pauseText.setString("Paused");
+        pauseText.setCharacterSize(50);
+        pauseText.setFillColor(sf::Color::White);
+        pauseText.setPosition(window.getSize().x / 2 - pauseText.getGlobalBounds().width / 2, 100);
+
+        continueText.setFont(font);
+        continueText.setString("Press [C] to Continue");
+        continueText.setCharacterSize(30);
+        continueText.setFillColor(sf::Color::Green);
+        continueText.setPosition(window.getSize().x / 2 - continueText.getGlobalBounds().width / 2, 200);
+
+        quitText.setFont(font);
+        quitText.setString("Press [Q] to Quit");
+        quitText.setCharacterSize(30);
+        quitText.setFillColor(sf::Color::Red);
+        quitText.setPosition(window.getSize().x / 2 - quitText.getGlobalBounds().width / 2, 250);
+
+        background.setSize(sf::Vector2f(window.getSize()));
+        background.setFillColor(sf::Color(0, 0, 0, 150)); // Pó³przezroczyste t³o
+    }
+
+    void draw(sf::RenderWindow& window) {
+        window.draw(background);
+        window.draw(pauseText);
+        window.draw(continueText);
+        window.draw(quitText);
+    }
+};
