0b8e99c19d6a55f1d6e82f993976f38521f0c760
s199112 <s199112@student.pg.edu.pl>
2025-01-19 03:25:34 +0100

    Update Pause.cpp

--- Gra/Pause.cpp
+++ Gra/Pause.cpp
@@ -8,6 +8,8 @@ private:
     sf::Text pauseText;
     sf::Text continueText;
     sf::Text quitText;
+    sf::Text saveText;
+    sf::Text loadText;
 
     sf::RectangleShape background;
 
@@ -35,6 +37,18 @@ public:
         quitText.setFillColor(sf::Color::Red);
         quitText.setPosition(window.getSize().x / 2 - quitText.getGlobalBounds().width / 2, 250);
 
+        saveText.setFont(font);
+        saveText.setString("Press [S] to Save");
+        saveText.setCharacterSize(30);
+        saveText.setFillColor(sf::Color::Cyan);
+        saveText.setPosition(window.getSize().x / 2 - saveText.getGlobalBounds().width / 2, 300);
+
+        loadText.setFont(font);
+        loadText.setString("Press [L] to LOAD");
+        loadText.setCharacterSize(30);
+        loadText.setFillColor(sf::Color::Blue);
+        loadText.setPosition(window.getSize().x / 2 - loadText.getGlobalBounds().width / 2, 350);
+
         background.setSize(sf::Vector2f(window.getSize()));
         background.setFillColor(sf::Color(0, 0, 0, 150)); // Pó³przezroczyste t³o
     }
@@ -44,5 +58,7 @@ public:
         window.draw(pauseText);
         window.draw(continueText);
         window.draw(quitText);
+        window.draw(saveText);
+        window.draw(loadText);
     }
 };
